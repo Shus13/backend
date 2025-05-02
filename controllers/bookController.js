0253cd864dc163cdf052461b75cb760a42f3e5bf
Shus13 <sushitkarki234@gmail.com>
2025-05-02 21:08:36 +0545

    adding contorllers files

---
+++ controllers/bookController.js
@@ -0,0 +1,46 @@
+
+const { books } = require("./database/connection")
+
+exports.fetchBooks = async function(req,res) {
+    // logic to fetch books from database
+    const datas = await books.findAll()
+    res.json ({
+        message: "Book fetched successfully",
+        datas
+    })
+}
+
+exports.addBooks = async function(req,res) {
+    // logic to add book to database goes here...
+    console.log(req.body)
+    // const bookName = req.body.bookName
+    // const bookPrice = req.body.bookPrice
+
+    const {bookName, bookPrice, bookAuthor, bookGenre} = req.body
+    await books.create({
+        bookName,
+        bookPrice,
+        bookAuthor,
+        bookGenre
+
+    })
+    // console.log(bookName)
+    // console.log(bookPrice)
+    res.json ({
+        message : "Book added successfully"
+    })
+}
+
+exports.deleteBooks = function(req,res){
+    // logic to delete book
+    res.json({
+        message : "Book added successfully"
+    })
+}
+
+exports.editBooks = function(req,res){
+    // logic to update book
+    res.json({
+        message : "Book updated successfully"
+    })
+}
