5fa8cf999d8e56e9d163bb78f636bf75ac8dd4fe
Shus13 <sushitkarki234@gmail.com>
2025-05-04 18:43:36 +0545

    Did exports

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -1,11 +1,13 @@
-const { fetchBooks, addBooks, deleteBooks, editBooks } = require("../controllers/bookController")
 
 
-const router = require("express").Router
+const { fetchBooks, addBooks, deleteBooks, editBooks, singleFetchBook } = require("../controllers/bookController")
+
+
+const router = require("express").Router()
 
 
 router.route("/books").get(fetchBooks).post(addBooks)
-router.route("/books/:id").delete(deleteBooks).patch(editBooks)
+router.route("/books/:id").delete(deleteBooks).patch(editBooks).get(singleFetchBook)
 
 
 
